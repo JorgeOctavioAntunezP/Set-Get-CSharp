3fc68f73a060e7ef6e9c149763a130108331a18d
JorgeOctavioAntunezP <jorge.oap18@gmail.com>
2022-05-20 11:42:41 -0700

    Programa de Excepcion
    Programa de Excepcion de la clase 20/05/2022

---
+++ Program_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingresa un numero: ");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingresa otro numero: ");
+                int n2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(n1 / n2);
+                Console.ReadLine();
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+        }
+    }
+}
